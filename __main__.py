232f4d258c9394604e3871b8fcaab58a5d0060b4
Kimmirut <zlksforwork14584@outlook.com>
2024-06-08 11:56:00 +0300

    Now CLI is working in current folder

--- __main__.py
+++ __main__.py
@@ -1,8 +1,20 @@
+import json
 import sys
 import os
 import glob
 from src.comp_alorythms import *
 
+
+# Command pallet arguments reading.
+
+# To use:
+# archiver <file_name> <method>
+
+# Where:
+# file_name - name of file that you want to compress/decompress.
+# method - action that should be aplied to chosen file, can be:
+#     - compress
+#     - decompress
 
 if len(sys.argv) != 3:
     exit(print('Введите название файла и название методаа чтобы применить'))
@@ -15,12 +27,15 @@ if file_name not in glob.glob1('.', file_name):
 if method not in ('compress, decompress'):
     exit(print('Введите корректное имя метода'))
 
+
+# Compressing file
+
 if method == 'compress':
 
     with open(file_name) as file:
 
         file = file.read()
-        methods = []
+        methods = []  # list of methods needed to decompress file, filling during compression.
 
         if method == 'compress':
             if len(file) > 1000:
@@ -30,35 +45,44 @@ if method == 'compress':
                 compressed = file
 
             compressed, codes = Huffman_encode(compressed)
-            methods.append('Huffman_decode')
 
-            print('compressed:\n' + str(compressed))
+
             # Creating folder with compressed files.
 
             compressed_dir_name = file_name + '.compressed'
             os.makedirs(compressed_dir_name)
 
             with open(f'{compressed_dir_name}/methods.txt', 'w') as methods_file,\
-                open(f'{compressed_dir_name}/compressed.bin', 'wb') as compressed_file:
+                 open(f'{compressed_dir_name}/compressed.bin', 'wb') as compressed_file,\
+                 open(f'{compressed_dir_name}/Huffman_codes.json', 'w') as codes_file:
 
                 compressed_file.write(bytes(compressed, 'utf-8'))
 
                 methods_file.writelines(methods)
 
+                json.dump(codes, codes_file)
+
 if method == 'decompress':
+
     archive = file_name
-    with open(f'{archive}.compressed/compressed.bin') as compressed_file,\
-         open(f'{archive}.compressed/methods.txt') as methods_file:
+    with open(f'{archive}/compressed.bin') as compressed_file,\
+         open(f'{archive}/methods.txt') as methods_file,\
+         open(f'{archive}/Huffman_codes.json') as codes_file:
 
         compressed_data = compressed_file.read()
+
+        codes = json.load(codes_file)
+        compressed_data = Huffman_decode(compressed_data, codes)
+
         methods = methods_file.read().split('\n')[:-1]
 
         for method in methods:
             compressed_data = eval(method)(compressed_data)
 
-        print(compressed_data)
+    with open(file_name.removesuffix('.compressed'), 'w') as decompressed_file:
+        decompressed_file.write(compressed_data)
 
 
 
 
-# & C:/Users/зелим/AppData/Local/Programs/Python/Python312/python.exe c:/Users/зелим/Desktop/My-archiver test1 decompress
+# & C:/Users/зелим/AppData/Local/Programs/Python/Python312/python.exe c:/Users/зелим/Desktop/My-archiver test1.compressed decompress
